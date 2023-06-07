b4a331cbc0317d08e69c65850984545c129ceb2a
frankogenrwoth <ogenrwoth.frank@gmail.com>
2023-06-07 22:16:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_module",
+    name="pyfirebasehandler",
     version="0.1.0",
     description="A Python module for handling a Firebase database",
-    author="Your Name",
-    author_email="your@email.com",
+    author="OGENRWOTH JIM FRANK",
+    author_email="ogenrwoth.frank@email.com",
     packages=find_packages(),
     install_requires=[
         "firebase-admin",
