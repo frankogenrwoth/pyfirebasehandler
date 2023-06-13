3434fc2f00e0261de218511cad7b02cb3ac8524d
Ogenrwoth frank <121643701+frankogenrwoth@users.noreply.github.com>
2023-06-13 20:39:37 +0000

    change setup py add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pyfirebasehandler",
     version="0.1.0",
     description="A Python module for handling a Firebase database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="OGENRWOTH JIM FRANK",
     author_email="ogenrwoth.frank@email.com",
     packages=find_packages(),
