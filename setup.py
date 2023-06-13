3d84b4c61d7d03550906fdb81125bcf9b25591a2
Ogenrwoth frank <121643701+frankogenrwoth@users.noreply.github.com>
2023-06-13 21:24:19 +0000

    configAll

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyfirebasehandler",
-    version="0.1.0",
+    version="1.0.0",
     description="A Python module for handling a Firebase database",
     long_description=long_description,
     long_description_content_type="text/markdown",
